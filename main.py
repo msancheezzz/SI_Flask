753f72e58a6d31c63f2175727222acfb70f3d370
msancheezzz <marcoschezchiz@gmail.com>
2023-04-11 16:50:39 +0200

    EJERCICIOS 1 y 2

---
+++ main.py
@@ -0,0 +1,146 @@
+import json
+import sqlite3
+import pandas as pd
+import matplotlib as mp
+from flask import Flask
+from flask import request
+
+###### CONEXIONES Y FICHEROS #########
+data = pd.read_csv("Data/alerts.csv")
+dev = open("data/devices.json")
+devices = json.load(dev)
+rows = data.shape[0]
+conexion = sqlite3.connect("bd1.db")
+cursor = conexion.cursor()
+
+######## CREACIÓN DE TABLAS #############
+conexion.execute("""create table if not exists articulos (
+                              fecha text,
+                              sid integer primary key,
+                              msg text,
+                              clasification text, 
+                              prioridad integer,
+                              protocolo text,
+                              origen text, 
+                              destino text, 
+                              puerto integer
+                        )""")
+conexion.execute("""create table if not exists dispositivos(
+                                  id text,
+                                  ip text primary key,
+                                  localizacion text,
+                                  responsable text,
+                                  analisis integer
+                            )""")
+cursor.execute("""create table if not exists responsables(
+                                      name text primary key,
+                                      telefono integer,
+                                      rol text
+                                )""")
+cursor.execute("""create table if not exists analisis(
+                                      id integer,
+                                      ip integer,
+                                      puertos_abiertos text,
+                                      n_puertos_abiertos integer,
+                                      servicios integer,
+                                      servicios_inseguros integer,
+                                      vulnerabilidades_detectadas integer,
+                                      primary key(puertos_abiertos, n_puertos_abiertos, servicios, servicios_inseguros, vulnerabilidades_detectadas)
+                                )""")
+
+############## RELLENAR TABLAS #############################3
+
+analisis_id = 0
+for a in devices:
+        responsable = a['responsable']
+        cursor.execute("INSERT OR IGNORE INTO responsables (name,telefono,rol) VALUES(?,?,?)", (responsable['nombre'], responsable['telefono'], responsable['rol']))
+        analisis = a['analisis']
+        if analisis["puertos_abiertos"] == 'None':
+            aux = 0
+        else:
+            aux = len(analisis["puertos_abiertos"])
+
+        cursor.execute("INSERT OR IGNORE INTO analisis (id, ip, puertos_abiertos, n_puertos_abiertos, servicios, servicios_inseguros, vulnerabilidades_detectadas) VALUES(?,?,?,?,?,?,?)", (analisis_id, a['ip'],json.dumps(analisis['puertos_abiertos']), aux, analisis['servicios'], analisis['servicios_inseguros'], analisis['vulnerabilidades_detectadas']))
+        cursor.execute("INSERT OR IGNORE INTO dispositivos (id,ip,localizacion,responsable,analisis) VALUES(?,?,?,?,?)", (a['id'],a['ip'],a['localizacion'],responsable['nombre'],analisis_id))
+        ##devices.to_sql("dispositivos", conexion, if_exists="replace", index=False)
+        analisis_id+=1
+
+data.to_sql("articulos", conexion, if_exists="replace", index=False)
+
+conexion.close()
+
+def flask():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return '''
+            <h1>FUFA</h1>
+            <form action="/DispositivosProblematicos" method="POST">
+                <input type="number" id="numero" name="numero">
+                <button type="submit">ENVIAR</button>
+            </form>
+            <ul></ul>
+            <form action="/IPProblematicas" method="POST">
+                <input type="number" id="numero2" name="numero2">
+                <button type="submit">ENVIAR</button>
+            </form>
+            <ul></ul>
+            <form action="/DispositivosPeligrosos" method="POST">
+                <input type="number" id="numero3" name="numero3">
+                <button type="submit">ENVIAR</button>
+            </form>
+        '''
+    @app.route('/DispositivosProblematicos', methods=["POST"])
+    def DispositivosProblematicos():
+        numero = int(request.form['numero'])
+        con = sqlite3.connect("bd1.db")
+        curs = con.cursor()
+        curs.execute("SELECT analisis.vulnerabilidades_detectadas, dispositivos.id FROM dispositivos INNER JOIN analisis ON analisis.id=dispositivos.analisis ORDER BY analisis.vulnerabilidades_detectadas DESC LIMIT {}".format(numero))
+        result = curs.fetchall()
+        html = f'<h1>Dispositivos más problematicos</h1>'
+        html += f'<ul>'
+        for a in result:
+            html += f'<li>{a[0]} {a[1]}</li>'
+        html += f'</ul>'
+        con.close()
+        return html
+
+    @app.route('/IPProblematicas', methods=["POST"])
+    def ipProblematicas():
+        numero = int(request.form['numero2'])
+        con = sqlite3.connect("bd1.db")
+        curs = con.cursor()
+        curs.execute(
+            "SELECT origen, COUNT(*) as num FROM articulos WHERE prioridad=1 GROUP BY origen ORDER BY num DESC LIMIT {}".format(
+                numero))
+        result = curs.fetchall()
+        html = f'<h1>IPs más problematicas</h1>'
+        html += f'<ul>'
+        for a in result:
+            html += f'<li>{a[0]} {a[1]}</li>'
+        html += f'</ul>'
+        con.close()
+        return html
+
+    @app.route('/DispositivosPeligrosos', methods=["POST"])
+    def dispositivosPeligrosos():
+        numero = int(request.form['numero3'])
+        con = sqlite3.connect("bd1.db")
+        curs = con.cursor()
+        curs.execute(
+            "SELECT dispositivos.id, ROUND(CAST(analisis.servicios_inseguros AS FLOAT)/ analisis.servicios * 100, 2) AS porcentaje FROM dispositivos INNER JOIN analisis ON analisis.id=dispositivos.analisis WHERE analisis.servicios>0 and porcentaje>33 ORDER BY porcentaje DESC LIMIT {}".format(
+                numero))
+
+        result = curs.fetchall()
+        html = f'<h1>Dispositivos más preligrosos</h1>'
+        html += f'<ul>'
+        for a in result:
+            html += f'<li>{a[0]} {a[1]}</li>'
+        html += f'</ul>'
+        con.close()
+        return html
+
+    app.run(debug=True)
+
+flask()
